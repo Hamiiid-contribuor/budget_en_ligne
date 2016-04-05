7c3cb37e9cb9e225e6e54d9717d893c81479acc1
hamid Lidrissi <lidrissihamid@gmail.com>
2016-04-05 18:05:26 +0000

    ma premier commit en neatbeans ,budget_en_ligne genereted

---
+++ src/java/service/EntiteAdministratifFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import bean.EntiteAdministratif;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hamid
+ */
+@Stateless
+public class EntiteAdministratifFacade extends AbstractFacade<EntiteAdministratif> {
+
+    @PersistenceContext(unitName = "budget_fstg_en_lignePU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EntiteAdministratifFacade() {
+        super(EntiteAdministratif.class);
+    }
+    
+}
